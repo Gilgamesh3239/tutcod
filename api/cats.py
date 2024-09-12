3a514907c90889c6f7fa60b712915a6e0659d6bc
Kirill <kencikpie@gmail.com>
2024-09-12 21:19:59 +0400

    initial commit

---
+++ api/cats.py
@@ -0,0 +1,29 @@
+import requests
+import os
+import uuid
+
+async def get_cat():
+    uri = "https://cataas.com/cat/gif"
+    response = requests.get(uri)
+    path = str(uuid.uuid4())
+    if response.status_code == 200:
+        with open("media/test.gif","wb") as f:
+            f.write(response.ocontent)
+        return f"media/test.gif"
+    else:
+        return None
+    
+
+def delete_cat(file_path):
+    print("DELETEING FILE :")
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        print("DELETED")
+
+if __name__ == "__main__":
+    # cat_gif = get_cat()
+    # if cat_gif:
+    #     print(f"Cat gif saved to {cat_gif}")
+    # else:
+        # print("Failed to fetch cat gif")
+    print("Testing")
---
+++ main.py
@@ -0,0 +1,43 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from aiogram.types import FSInputFile
+
+from api.cats import delete_cat, get_cat
+
+
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="7367658102:AAElBhmFEu_GHFXwRrfsp9zdzDbS0kemiDw")
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    
+    await message.answer("Hello!")
+
+@dp.message(Command("gifcat"))
+async def cmd_start(message: types.Message):
+    gif_path = await get_cat()
+    print(f"[!] Donloaded image {gif_path}")
+    await bot.send_animation(
+        message.chat.id, 
+        FSImputFile(gif_path), 
+    )
+    print("[!] Uploaded")
+    delete_cat(gif_path)
+    await message.answer("meow")
+
+    
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
